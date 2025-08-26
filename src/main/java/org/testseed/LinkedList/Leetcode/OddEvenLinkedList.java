0ee2fa03b291311daf47116442ae584790a2cc04
yogeshwankhede007 <yogesh.wankhede@syngenta.com>
2025-08-26 19:58:14 +0530

    odd even linkedlist re-link

---
+++ src/main/java/org/testseed/LinkedList/Leetcode/OddEvenLinkedList.java
@@ -0,0 +1,20 @@
+package org.testseed.LinkedList.Leetcode;
+
+public class OddEvenLinkedList {
+    public ListNode oddEvenList(ListNode head) {
+
+        if(head == null || head.next == null) return head;
+
+        ListNode odd = head, even = head.next, evenHead = even;
+
+        while(even != null && even.next != null){
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+        return head;
+    }
+}
---
+++ src/main/java/org/testseed/LinkedList/RemoveElements01.java
@@ -0,0 +1,4 @@
+package org.testseed.LinkedList;
+
+public class RemoveElements01 {
+}
